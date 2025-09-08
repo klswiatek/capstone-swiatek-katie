9f905937aba73866c69d5943f8864ce453456551
klswiatek <klswiatek@yahoo.com>
2025-09-07 23:02:32 -0400

    Add current site version

---
+++ orangescript.js
@@ -0,0 +1,33 @@
+function refreshColors () {
+
+var url = "http://colormind.io/api/";
+var data = {
+	model : "default",
+	input : [[255,165,0],"N","N","N","N"]
+}
+
+var http = new XMLHttpRequest();
+
+http.onreadystatechange = function() {
+    if (http.readyState == 4 && http.status == 200) {
+        
+        var response = JSON.parse(http.responseText);
+        var palette = response.result;
+
+    
+        var colors = document.querySelectorAll('.colors');
+
+        palette.forEach(function(rgb, index) {
+            if (colors) {
+                colors.style.backgroundColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+            }
+        });
+    }
+};
+
+http.open("POST", url, true);
+http.send(JSON.stringify(data));
+
+}
+
+document.addEventListener('DOMContentLoaded', refreshColors);
