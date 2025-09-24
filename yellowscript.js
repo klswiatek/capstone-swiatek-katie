d7a8f4275590485829cdcd56f7943a3e000e9f96
klswiatek <klswiatek@yahoo.com>
2025-09-24 19:02:38 -0400

    Second yellow row added

--- yellowscript.js
+++ yellowscript.js
@@ -31,3 +31,37 @@ http.send(JSON.stringify(data));
 }
 
 document.addEventListener('DOMContentLoaded', refreshColors);
+
+function newColors () {
+
+    var url = "http://colormind.io/api/";
+    var data = {
+        model : "default",
+        input : [[255,255,103],"N","N","N","N"]
+    }
+    
+    var http = new XMLHttpRequest();
+    
+    http.onreadystatechange = function() {
+        if (http.readyState == 4 && http.status == 200) {
+            
+            var response = JSON.parse(http.responseText);
+            var palette = response.result;
+    
+        
+            var colors = document.querySelectorAll('.colors2');
+    
+            palette.forEach(function(rgb, index) {
+                if (colors[index]) {
+                    colors[index].style.backgroundColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+                }
+            });
+        }
+    };
+    
+    http.open("POST", url, true);
+    http.send(JSON.stringify(data));
+    
+    }
+    
+    document.addEventListener('DOMContentLoaded', newColors);
