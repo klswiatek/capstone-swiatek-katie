0fadf5f7cd508cc87d9003d5ddb7ffe4a34ac13f
klswiatek <klswiatek@yahoo.com>
2025-09-24 19:17:34 -0400

    yellow lines added, home button added

--- yellowscript.js
+++ yellowscript.js
@@ -65,3 +65,37 @@ function newColors () {
     }
     
     document.addEventListener('DOMContentLoaded', newColors);
+
+    function nextColors () {
+
+        var url = "http://colormind.io/api/";
+        var data = {
+            model : "default",
+            input : [[254,255,191],"N","N","N","N"]
+        }
+        
+        var http = new XMLHttpRequest();
+        
+        http.onreadystatechange = function() {
+            if (http.readyState == 4 && http.status == 200) {
+                
+                var response = JSON.parse(http.responseText);
+                var palette = response.result;
+        
+            
+                var colors = document.querySelectorAll('.colors3');
+        
+                palette.forEach(function(rgb, index) {
+                    if (colors[index]) {
+                        colors[index].style.backgroundColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+                    }
+                });
+            }
+        };
+        
+        http.open("POST", url, true);
+        http.send(JSON.stringify(data));
+        
+        }
+        
+        document.addEventListener('DOMContentLoaded', newColors);
