33ca661fcbc1c4a203931167a36b5bccd43f7c72
klswiatek <klswiatek@yahoo.com>
2025-09-27 20:09:52 -0400

    Green color lines added

--- greenscript.js
+++ greenscript.js
@@ -3,7 +3,7 @@ function refreshColors () {
 var url = "http://colormind.io/api/";
 var data = {
 	model : "default",
-	input : [[0,255,0],"N","N","N","N"]
+	input : [[139,194,74],"N","N","N","N"]
 }
 
 var http = new XMLHttpRequest();
@@ -31,3 +31,71 @@ http.send(JSON.stringify(data));
 }
 
 document.addEventListener('DOMContentLoaded', refreshColors);
+
+function newColors () {
+    
+    var url = "http://colormind.io/api/";
+    var data = {
+        model : "default",
+        input : ["N","N",[51,103,30],"N","N"]
+    }
+    
+    var http = new XMLHttpRequest();
+    
+    http.onreadystatechange = function() {
+        if (http.readyState == 4 && http.status == 200) {
+            
+            var response = JSON.parse(http.responseText);
+            var palette = response.result;
+    
+        
+            var colors = document.querySelectorAll('.colors2');
+    
+            palette.forEach(function(rgb, index) {
+                if (colors[index]) {
+                    colors[index].style.backgroundColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+                }
+            });
+        }
+    };
+    
+    http.open("POST", url, true);
+    http.send(JSON.stringify(data));
+    
+    }
+    
+    document.addEventListener('DOMContentLoaded', newColors);
+
+    function nextColors () {
+
+        var url = "http://colormind.io/api/";
+        var data = {
+            model : "default",
+            input : ["N","N","N","N",[217,233,199]]
+        }
+        
+        var http = new XMLHttpRequest();
+        
+        http.onreadystatechange = function() {
+            if (http.readyState == 4 && http.status == 200) {
+                
+                var response = JSON.parse(http.responseText);
+                var palette = response.result;
+        
+            
+                var colors = document.querySelectorAll('.colors3');
+        
+                palette.forEach(function(rgb, index) {
+                    if (colors[index]) {
+                        colors[index].style.backgroundColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+                    }
+                });
+            }
+        };
+        
+        http.open("POST", url, true);
+        http.send(JSON.stringify(data));
+        
+        }
+        
+        document.addEventListener('DOMContentLoaded', newColors);
